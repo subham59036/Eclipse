f911cf5ff2a4504547f4671b8a264cd70e463f5b
Subham Saha <subham59036@gmail.com>
2026-07-06 19:30:35 +0530

    Updated repo to modern version

---
+++ backend/src/test/java/com/eclipse/backend/EclipseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclipse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
